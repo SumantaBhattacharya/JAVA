b754350f4a28123fc97cfc6da8dcd8bd21d5f98f
SumantaBhattacharya <sumanta2004@gmail.com>
2025-01-24 19:51:52 +0530

    Add program to demonstrate enhanced for loop with arrays
    - Initialize an integer array with values
    - Use an enhanced for loop to iterate through the array
    - Print each element of the array

---
+++ ArrayL4ach.java
@@ -0,0 +1,12 @@
+
+public class ArrayL4ach {
+    public static void main(String[] args) {
+        int [] arr = new int[] {10,20,30,40,50};
+// systax for(datatype for_each_variable: name_of_array)
+        for (int num : arr){
+            System.out.println(num);
+        }
+
+    }
+}
+
