9e7cb2ccd9ab93b4db10b7f01412879165843409
SumantaBhattacharya <sumanta2004@gmail.com>
2025-01-25 18:38:11 +0530

    Hierarchical inheritance refers to a child and parent class (IS-A)relationship were more than one classes extends the same class.  * child1-->parent class <--child3  * child3--->

---
+++ Hierar_inheritA.java
@@ -0,0 +1,9 @@
+/*
+Hierarchical inheritance refers to a child and parent class (IS-A)relationship were more than one classes extends the same class.
+ * child1-->parent class <--child3
+ * child3--->
+*/
+public class Hierar_inheritA {
+    int claws=4;
+    String Domestic = "pet animal";
+}
