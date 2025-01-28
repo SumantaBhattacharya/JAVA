4095c7d2c7773fbe4b25fb978780806385168868
SumantaBhattacharya <sumanta2004@gmail.com>
2025-01-28 18:11:11 +0530

    Add multiple new Java classes and initial implementations

--- A_1.java
+++ A_1.java
@@ -1,39 +1,42 @@
 
-interface Calculator {
+    
+
+interface Calculator{
     public abstract void add();
-
     abstract void sub();
-
-    default void init() {
-        System.out.println("This is a default method");
-    }
-}
-
-class Mycal1 implements Calculator {
-    @Override
-    public void add() {
-        int a = 6;
-        int b = 9;
-        int c = a + b;
-        System.out.println("Addition result in Mycal1: " + c);
-    }
-
-    @Override
-    public void sub() {
-        int a = 6;
-        int b = 9;
-        int c = a - b;
-        System.out.println("Subtraction result in Mycal1:" + c);
-    }
-}
-
-public class A_1 {
-    public static void main(String[] args) {
-
-        Mycal1 T1 = new Mycal1();
-
-        T1.add();
-        T1.sub();
-
-    }
-}
+    
+    default void init(){ 
+    System.out.println("This is a default method");}
+ }
+ 
+ class Mycal1 implements  Calculator {
+     @Override
+     public void add() {
+         int a = 6;
+         int b = 9;
+         int c = a + b;
+         System.out.println("Addition result in Mycal1: " + c);
+     }
+ 
+     @Override
+     public void sub() {
+         int a = 6;
+         int b = 9;
+         int c = a - b;
+         System.out.println("Subtraction result in Mycal1:" + c);
+     }
+ }
+ 
+ 
+ public class A_1 {
+     public static void main(String[] args) {
+        
+         Mycal1 T1 = new Mycal1();
+ 
+         T1.add();
+         T1.sub();
+         T1.init();
+ 
+     
+     }
+ }
