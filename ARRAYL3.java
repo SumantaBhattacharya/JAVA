4d1e40b04e447af487040dc17b7528a38745caa4
SumantaBhattacharya <sumanta2004@gmail.com>
2025-01-29 16:29:49 +0530

    Add ARRAYL3 class for demonstration of integer array initialization and output

---
+++ ARRAYL3.java
@@ -0,0 +1,18 @@
+
+
+public class ARRAYL3 {
+    public static void main(String[] args) {
+       
+      
+          int [] arr = new int[] {10,20,30,40,50};
+
+           
+// output
+         for (int i=0;i<arr.length;i++) {
+            System.out.println(arr[i]);
+        }
+
+    
+    }
+
+}
