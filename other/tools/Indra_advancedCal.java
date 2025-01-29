3138dc7d4d195238e2b86a85951d28a4a5fabf8c
SumantaBhattacharya <sumanta2004@gmail.com>
2025-01-29 16:28:40 +0530

    Add Indra_advancedCal class with multiplication, division, and modulus methods

---
+++ other/tools/Indra_advancedCal.java
@@ -0,0 +1,19 @@
+package other.tools;
+//import tools.Indra_cal;
+
+public class Indra_advancedCal extends Indra_cal{
+    public int mul (int x,int y){
+        return x*y;
+
+    }
+    public int div (int x,int y){
+        return x/y;
+        
+    }
+
+    public float mod(float x,float y){
+        return x%y;
+        
+    }
+}
+
