8cc0e7b0fb18b1ef07b1a90076f06f55fc5dd351
SumantaBhattacharya <sumanta2004@gmail.com>
2025-01-29 16:30:47 +0530

    Add Encap_Oops2 class to demonstrate encapsulation with object creation and method calls

---
+++ Encap_Oops2.java
@@ -0,0 +1,24 @@
+public class Encap_Oops2 {
+    // creating a object
+    public static void main(String[] args) {
+        
+    
+    Encap_Oops T1 = new Encap_Oops ();
+    //T1.name = "sumanta bhattacharya";
+ //T1.age=19;
+// fuction calling
+    T1.setName("Sumanta");
+    T1.setAge(19);
+    System.out.println(T1.getName());
+    System.out.println(T1.getAge());
+
+    Encap_Oops T2 = new Encap_Oops ();
+    T2.setName("Arin");
+    T2.setAge(18);
+    T2.display();
+
+
+
+    
+}
+} 
