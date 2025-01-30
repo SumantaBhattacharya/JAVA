941d313abf59957d720846908bc05cd01a73c54d
SumantaBhattacharya <sumanta2004@gmail.com>
2025-01-30 16:25:30 +0530

    Add M class to demonstrate calculating the remainder of two integers

---
+++ M.java
@@ -0,0 +1,12 @@
+public class M {
+    public static void main(String[] args) {
+        
+
+    int a = 5;
+    int b = 3;
+    int c = a % b;
+
+System.out.println("The remainder is: " + c);
+
+}
+}
