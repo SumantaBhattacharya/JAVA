693ecd10016a4449c9b918e73453d2b08f0f9116
SumantaBhattacharya <sumanta2004@gmail.com>
2025-01-30 16:30:06 +0530

    Add Hierar_inheritC class to demonstrate inheritance and method implementation

---
+++ Hierar_inheritC.java
@@ -0,0 +1,13 @@
+// cat
+public class Hierar_inheritC extends Hierar_inheritA {
+    String name="Muse";
+    int costing=2000;
+
+   
+ public void Meow(){
+    System.out.println("cat are so cute");
+ }
+ public void eat(){
+    System.out.println("A cat likes to have Milk");
+ }
+}
