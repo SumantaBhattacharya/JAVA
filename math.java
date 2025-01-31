673efd24c657ff4cdddcd21c4d304c5da724a3d0
SumantaBhattacharya <sumanta2004@gmail.com>
2025-01-31 14:25:58 +0530

    Add math class to demonstrate basic mathematical operations using the Math library

---
+++ math.java
@@ -0,0 +1,19 @@
+
+public class math {
+    public static void main(String[] args) {
+        double num = Math.sqrt(16);
+        System.out.println(num);
+
+        int num1=Math.min(87,98);//The Math class should start with a capital "M," not a lowercase "m."
+        System.out.println(num1);
+
+         int num3=Math.max(87,98);
+        System.out.println(num3);
+
+        double num2=Math.random();
+        System.out.println(num2);
+
+        
+        System.out.println((int)(Math.random()*100));
+    }
+}
