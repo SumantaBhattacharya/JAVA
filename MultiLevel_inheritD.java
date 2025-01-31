b1011c824f85f28e3490aabd3db85b69a6df69dc
SumantaBhattacharya <sumanta2004@gmail.com>
2025-01-31 14:27:19 +0530

    Add MultiLevel_inheritD class to demonstrate multi-level inheritance with dog-specific behaviors

---
+++ MultiLevel_inheritD.java
@@ -0,0 +1,20 @@
+// Dog
+public class MultiLevel_inheritD extends MultiLevel_inheritA {
+
+   String colour;
+   String breed;
+   String name = "bankie";
+
+   // methods
+   public void wagTail() {
+      System.out.println("dog usually wag their tail");
+   }
+
+   public void Bark() {
+      System.out.println("dog barking is a sign of hes anger");
+   }
+
+   public void eat() {
+      System.out.println("dog like to eat bones");
+   }
+}
