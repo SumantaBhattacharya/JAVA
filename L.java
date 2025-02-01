35d3c4ea535b51bfa8755fedf9201b58bfe62d7d
SumantaBhattacharya <sumanta2004@gmail.com>
2025-02-01 14:31:38 +0530

    Add L class to implement a simple program that prints a smiley face in an infinite loop

---
+++ L.java
@@ -0,0 +1,11 @@
+// Import the Arrays class
+public class L{
+    public static void main(String[] args) {
+        
+        while (true) { // Replace '5' with the desired number of repetitions
+            System.out.println(":D");
+            
+            
+        }
+    }
+}
