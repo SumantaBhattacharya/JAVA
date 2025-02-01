5ee7e54d87e1bca3e14cb32f898dda0ba6a59f86
SumantaBhattacharya <sumanta2004@gmail.com>
2025-02-01 14:31:19 +0530

    Add MultiLevel_inherit4 class to demonstrate multi-level inheritance with a simple example

---
+++ MultiLevel_inherit4.java
@@ -0,0 +1,9 @@
+public class MultiLevel_inherit4 {
+    public static void main(String[] args) {
+        MultiLevel_inheritL T1 = new MultiLevel_inheritL();
+        System.out.println("We adopted " + T1.name + " in 1965 ");
+        System.out.println("labrador is a"+T1.Adaptibility1+"which posses "+T1.claw+" claws");
+        T1.Bark();
+    }
+}
+
