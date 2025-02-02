6312f60f63fd7207a74efa221087071de148aece
SumantaBhattacharya <sumanta2004@gmail.com>
2025-02-02 10:35:43 +0530

    Add ImplicitC class to demonstrate implicit and explicit type casting in Java

---
+++ ImplicitC.java
@@ -0,0 +1,13 @@
+public class ImplicitC {
+    public static void main(String[] args) {
+        int a=1;
+        double c=6.9; 
+
+        double b=a;//implicit casting
+         int a1 = (int)c;//explicit casting
+
+        System.out.println(b);
+        System.out.println(a1);
+    }
+    
+}
