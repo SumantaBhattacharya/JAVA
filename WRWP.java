61140b9a78f0595ba291530963ff0de1d235c682
SumantaBhattacharya <sumanta2004@gmail.com>
2025-02-02 10:34:40 +0530

    Add WRWP class to demonstrate a simple addition operation in Java

---
+++ WRWP.java
@@ -0,0 +1,19 @@
+
+public class WRWP {
+
+    static int addtwonum(int x , int y){
+    int z= x+y;
+     return (z) ;
+    }
+
+    public static void main(String[] args) {
+        int sum;
+        int a=78;
+        
+      
+        sum= addtwonum(a, 98);
+  System.out.println(sum);
+
+  System.out.println(addtwonum(10,33));
+    }
+}
