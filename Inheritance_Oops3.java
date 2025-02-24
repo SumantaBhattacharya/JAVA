b5afe3eb87cda5278e38c7b6b47c6b3c65363a61
SumantaBhattacharya <sumanta2004@gmail.com>
2025-02-24 12:10:16 +0530

    Add Inheritance_Oops3 class to demonstrate inheritance with a Dog example in Java

---
+++ Inheritance_Oops3.java
@@ -0,0 +1,7 @@
+public class Inheritance_Oops3 {
+    public static void main(String[] args) {
+        InheritDog_Oops1 Bankie = new InheritDog_Oops1();
+        System.out.println("Dog has " +Bankie.eyes + " eyes.");
+        Bankie.eat();
+    }
+}
