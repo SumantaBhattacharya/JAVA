7c2621e869017da096dfa08e577fa68cfe80861d
SumantaBhattacharya <sumanta2004@gmail.com>
2025-02-24 12:13:08 +0530

    Add YCH_2D class to demonstrate the declaration and initialization of a 2D array in Java

---
+++ YCH_2D.java
@@ -0,0 +1,23 @@
+public class YCH_2D {
+    public static void main(String[] args) {
+        
+    
+
+ // syntax datatype [][] array_name = new datatype(constructor) [size of row] [size of column];
+ // in 2d array,Mentioning the size of column is not mandatory, but always Mention the row size ( if the declaration is looked like aboveline of code.)
+ 
+   
+ //1st Type
+ // number of raw is fixed
+ // Declaration and memory allocation of 2d array
+   int [][] arr= new int[3][3];//12 elements if we dont want to pass anything in row then we can also keep zero as the  null value
+ // value initialize in 2d array
+ arr [0][1]=10; 
+ System.out.println(arr[0][1]);
+ 
+ 
+
+
+    }
+
+}
