edf8a297aa9843a1377ba0b6fd74a240e2d48ce3
SumantaBhattacharya <sumanta2004@gmail.com>
2025-02-24 12:08:45 +0530

    Add Hierar_inheritD class to demonstrate inheritance with a Dog example in Java

---
+++ Hierar_inheritD.java
@@ -0,0 +1,15 @@
+//Dog
+public class Hierar_inheritD extends Hierar_inheritA  {
+    String name="Bankie";
+    int costing=16000;
+
+    public void wagTail(){
+        System.out.println("dog usually wag their tail");
+     }
+     public void Bark(){
+        System.out.println("dog barking is a sign of hes anger");
+     }
+     public void eat(){
+        System.out.println("dog like to eat bones");
+     }
+}
