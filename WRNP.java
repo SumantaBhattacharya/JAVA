389121b679fbf7ac5f2d0de592d76281dfdd330c
SumantaBhattacharya <sumanta2004@gmail.com>
2025-02-24 12:11:03 +0530

    Add WRNP class to demonstrate a method with return type and no parameters in Java

---
+++ WRNP.java
@@ -0,0 +1,15 @@
+public class WRNP {// with return type and no parameter 
+
+    static int addtwonum (){
+int x = 77;
+int y= 97;
+ int z = x+y ;
+    return  (z);
+ }
+    public static void main(String[] args) {
+       int sum;
+       sum=addtwonum();// when we use return we use this kind of method
+
+        System.out.println(sum);
+    }
+}
