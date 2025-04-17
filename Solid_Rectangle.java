cdcf4390b20324c1f23be4abbe10e1e5ad02fde5
SumantaBhattacharya <sumanta2004@gmail.com>
2025-04-17 11:58:56 +0530

    Add Solid_Rectangle.java to print a solid rectangle pattern using stars

---
+++ Solid_Rectangle.java
@@ -0,0 +1,97 @@
+/* PS C:\Users\SUDIP BHATTACHARYA\Desktop\JAVA 4 DSA>  & 'C:\Program Files\Java\jdk-22\bin\java.exe' '--enable-preview' '-XX:+ShowCodeDetailsIn
+ExceptionMessages' '-cp' 'C:\Users\SUDIP BHATTACHARYA\AppData\Roaming\Code\User\workspaceStorage\36e6ac498a2a10d67a6687f8692a124b\redhat.jav
+a\jdt_ws\JAVA 4 DSA_3a754290\bin' 'Solid_Rectangle'
+
+   SOLID RECTRANGLE
+
+   *****
+   *****
+   *****
+   ***** 
+
+
+ jodi ekta loop * eta print hoche tahole nischoi thar print ar ekta loop lagale tar bhitora print hobe thhats why
+ pattern problem is based on 2d plane x and y tahts why we only using 2 loops one inside another
+ the outer for loop is x-rows and the inner for loop is y -columns
+*/
+
+public class Solid_Rectangle{
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) { //nested loops 
+            // System.out.println("*****");
+            for(int j=1; j<=5;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
+/*  College life journey
+
+    1st sem
+    c basics                             -College
+    Digital Electronics                  -College
+    Soft Skills                          -College
+    Mathematics for Computing            -College
+
+
+    2nd sem
+    c basics                             -Youtube(Youth Career Hub) + Youtube(Programiz) 
+    python basics                        -Youtube(Youth Career Hub) + Youtube(Programiz)
+    
+    Computer Architecture                -College
+    Discrete Strcuture                   -College
+    Professional Communication           -College
+    Environmental Science                -College
+       
+    
+    3rd sem
+    dsa in python                        -Youtube(Amulya's academy)
+    java                                 -Youth Career Hub(Core java(Course-1500)) +
+    oops                                 -Youth Career Hub +
+    sql                                  -Youth Career Hub +
+    git & github                         -Youth Career Hub
+    markdown files                       -Youtube(MySirG)
+
+    Operating system                     -College
+    Climate Change and Health            -College
+
+    4th sem
+    html                                 -Youtube(Apna College)
+    css                                  -Youtube(Apna College)
+    js                                   -Youtube(Chai aur Code)
+    gsap                                 -Youtube(Sheryians Coding School)
+    locomotive scroller                  -Youtube(Sheryians Coding School)
+    swipper.js                           -Youtube(Sheryians Coding School)
+    node                                 -(Apna College(Delta(Course-5000))) +
+    Express                              -Apna College +
+    git & github                         -Apna College +
+    sql                                  -Apna College +
+    MongoDB                              -Apna College
+    cloudinary                           -Youtube(Chai aur Code)
+
+    Database Management System           -College
+    Software Engineering                 -College
+    Design and Analysis of Algorithms    -College
+    Project Management                   -College
+
+    plans-
+    DSA in JAVA
+    REACT JS
+    NEXT JS
+    TYPESCRIPT 
+    REACT NATIVE
+    Blockchain-web3
+    three js 
+
+    
+    testing
+    DevOps
+    Docker
+    Kubernet
+    AWS
+
+
+*/ 
